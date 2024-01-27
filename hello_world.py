728bd4f5434b1ddd16d2ce07989fb67f49bd3276
Zayne <zayne5504@hotmail.com>
2024-01-26 20:32:19 -0600

    added 2 files

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+name = input("What is your first name?  ")
+fi =  input("What is your first initial?  ")
+si =  input("What is your last initial?  ")
+n =  round(float(input("Give me a number  ")),2)
+m =  round(float(input("Give me another number  ")),2)
+print(name + "  "+ fi + si)
+q = n + m
+r = n - m
+s = round((n*m),2)
+t = round((n/m),2)
+print(str(n)+" + "+str(m)+" = "+str(q))
+print(str(n)+" - "+str(m)+" = "+str(r))
+print(str(n)+" * "+str(m)+" = "+str(s))
+print(str(n)+" / "+str(m)+" = "+str(t))
+#Zayne B.
+#this is a sample little assignment file from a random assignment from another course I previously took.
